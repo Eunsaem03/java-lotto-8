5e16bb202afddcc5b87f6d7b9a900b80afe48573
Eunsaem03 <eunsaem910@gmail.com>
2025-10-31 23:49:14 +0900

    refactor: InputView 코드 구조 개선
    - 입력 안내문 출력 순서를 수정함.
    - private 생성자 추가로 인스턴스 생성을 방지함.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,21 +4,23 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private InputView() {}
+
     public static String inputPurchaseAmount() {
-        String purchaseAmount = Console.readLine();
         System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = Console.readLine();
         return purchaseAmount;
     }
 
     public static String inputWinningNumber() {
-        String winningNumber = Console.readLine();
         System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumber = Console.readLine();
         return winningNumber;
     }
 
     public static String inputBonusNumber() {
-        String bonusNumber = Console.readLine();
         System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumber = Console.readLine();
         return bonusNumber;
     }
 }
