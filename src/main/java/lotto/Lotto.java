08049c5280ac0d14cac4b60ab5705aeac8d4007a
Eunsaem03 <eunsaem910@gmail.com>
2025-11-02 17:15:35 +0900

    feat: Lotto 클래스 추가 구현
    - 1~45 범위 내의 6개 숫자를 뽑는 generate 메서드를 추가함.
    - numbers 필드에 접근할 수 있도록 getNumbers 메서드를 추가함.
    - 로또 번호가 당첨 번호와 몇 개 일치하는 지 계산하는 matchCount 메서드를 추가함.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import lotto.domain.NumberGenerator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,9 +11,27 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public static Lotto generate() {
+        return new Lotto(NumberGenerator.generate());
+    }
+
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int matchCount(Lotto PurchaseNumber) {
+        int count = 0;
+
+        for (Integer number : PurchaseNumber.numbers) {
+            if (this.numbers.contains(number)) count++;
+        }
+
+        return count;
     }
 }
